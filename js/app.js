c99b05e9813a147320edd3386d469f074281904b
Raja Aravind Pulagam <dnivara@Rajas-MBP.attlocal.net>
2022-12-02 18:11:36 -0500

    changes to the redirect listener on keydown

--- js/app.js
+++ js/app.js
@@ -6,3 +6,7 @@ document.addEventListener('keydown', (event) => {
         window.location.href = 'maze.html'
     }
 });
+
+document.addEventListener('keyup', (event) => {
+    delete keysPressed[event.key];
+ });
